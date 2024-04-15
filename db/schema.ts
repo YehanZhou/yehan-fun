c83e6cbd6c25c2c380030822f18b781903a56ab0
01427766 <zhouyehan@sf-express.com>
2024-04-15 19:01:15 +0800

    feat: link actions

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,39 @@
-import { integer, text, boolean, pgTable } from "drizzle-orm/pg-core";
+import { integer, text, boolean, time, pgTable } from "drizzle-orm/pg-core";
+import { relations } from 'drizzle-orm';
 
 export const todo = pgTable("todo", {
   id: integer("id").primaryKey(),
   text: text("text").notNull(),
   done: boolean("done").default(false).notNull(),
 });
+
+
+export const category = pgTable("category", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  icon: text("icon").notNull(),
+  description: text("description").notNull(),
+  rank: integer("rank").notNull(),
+  createTime: time("create_time").default("now()").notNull(),
+  updateTime: time("update_time").default("now()").notNull(),
+});
+
+export const categoryRelations = relations(category, ({many}) => ({
+  links: many(link)
+}));
+
+export const link = pgTable("link", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  icon: text("icon").notNull(),
+  description: text("description").notNull(),
+  done: boolean("done").default(false).notNull(),
+  rank: integer("rank").notNull(),
+  createTime: time("create_time").default("now()").notNull(),
+  updateTime: time("update_time").default("now()").notNull(),
+  public: boolean("public").default(false).notNull(),
+  status: integer("status").default(1).notNull(),
+  categoryId: integer("category_id").notNull().references(()=>category.id),
+});
+
+
